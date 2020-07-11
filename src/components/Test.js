7c4652efcab87495c8f24416fa508bc479015342
arghac14 <01argha@gmail.com>
2020-07-11 21:53:11 +0530

    Lifecycle: Mounting

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,15 +3,26 @@ import React, {Component} from 'react';
 class Test extends Component {
     constructor(props){
         super(props);
-    
+    //const n = props.name;
+
     this.state = {
         name : 'arghac14',
         Age : 22
      }
-}
+    }
 
-    changeContent = () => {
-        this.setState({Age: 20});
+    static getDerivedStateFromProps(props, state) {
+        return {name: props.name};
+    }
+
+    componentDidMount() {
+        setTimeout(()=>{
+            this.setState({Age: 22.1})
+        }, 1000)
+    }
+
+    changeContent = (age) => {
+        this.setState({Age: age});
     }
 
     render(){
@@ -21,7 +32,7 @@ class Test extends Component {
               <h1>Hello World!</h1>
               <h1>I am {this.state.name}</h1>
               <h1>My age is {this.state.Age} </h1>
-              <button type="button" onClick = {this.changeContent}>Click Here</button>
+              <button type="button" onClick = {()=>this.changeContent(25)}>Click Here</button>
           </div>
       );
   }
